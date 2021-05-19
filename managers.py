2b74d8e1f135f6a19e4550b3a1fa8bce81c1d1bf
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-05-19 20:16:33 -0300

    chore: create CaptureManager constructor

--- managers.py
+++ managers.py
@@ -2,3 +2,20 @@ import time
 
 import cv2
 import numpy
+
+
+class CaptureManager(object):
+    def __init__(self, capture, previewWindowManager=None, shouldMirrorPreview=False):
+        self.previewWindowManager = previewWindowManager
+        self.shouldMirrorPreview = shouldMirrorPreview
+        self._capture = capture
+        self._channel = 0
+        self._enteredFrame = False
+        self._frame = None
+        self._imageFilename = None
+        self._videoFilename = None
+        self._videoEncoding = None
+        self._videoWriter = None
+        self._startTime = None
+        self._framesElapsed = 0
+        self._fdsEstimate = None
