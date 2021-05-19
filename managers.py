4d1ea90b61d085851932478e025fe7c0ede355f1
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-05-19 20:29:39 -0300

    chore: add getters and setters methods for the CaptureManager properties

--- managers.py
+++ managers.py
@@ -19,3 +19,28 @@ class CaptureManager(object):
         self._startTime = None
         self._framesElapsed = 0
         self._fdsEstimate = None
+
+    @property
+    def channel(self):
+        return self._channel
+
+    @channel.setter
+    def channel(self, value):
+        if self._channel != value:
+            self._channel = value
+            self._frame = None
+
+    @property
+    def frame(self):
+        if self._enteredFrame and self._frame is None:
+            _, self._frame = self._capture.retrieve(self._frame, self.channel)
+
+        return self._frame
+
+    @property
+    def isWritingImage(self):
+        return self._imageFilename is not None
+
+    @property
+    def isWritingVideo(self):
+        return self._videoFilename is not None
