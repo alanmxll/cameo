5785139111d2ff208182ffcdf5e8d66df21ee423
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-05-19 22:38:17 -0300

    chore: add method run

--- cameo.py
+++ cameo.py
@@ -8,3 +8,15 @@ class Cameo(object):
         self._windowManager = WindowManager('Cameo', self.onKeypress)
         self._captureManager = CaptureManager(
             cv2.VideoCapture(0), self._windowManager, True)
+
+    def run(self):
+        """Run the main loop."""
+        self._windowManager.createWindow()
+        while self._windowManager.isWindowCreated:
+            self._captureManager.enterFrame()
+            frame = self._captureManager.frame
+            if frame is not None:
+                # TODO: Filter the frame (Chapter 3).
+                pass
+            self._captureManager.exitFrame()
+            self._windowManager.processEvent()
