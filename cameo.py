d4f7d2af7ede3cbe87b265f5735b65385cfbe37b
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-05-19 22:35:43 -0300

    chore: create Cameo constructor

--- cameo.py
+++ cameo.py
@@ -1,3 +1,10 @@
 import cv2
 
 from managers import CaptureManager, WindowManager
+
+
+class Cameo(object):
+    def __init__(self):
+        self._windowManager = WindowManager('Cameo', self.onKeypress)
+        self._captureManager = CaptureManager(
+            cv2.VideoCapture(0), self._windowManager, True)
